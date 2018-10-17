9ad19d9362ef0a8e98efff3140fb1eb6f2335644
Ned Schwartz <ned@theinterned.net>
2018-10-17 14:11:02 -0400

    Updates

--- precache-manifest.d455fcedd279e41753eedc902eb8f228.js
+++ precache-manifest.d455fcedd279e41753eedc902eb8f228.js
@@ -4,15 +4,15 @@ self.__precacheManifest = [
     "url": "/static/js/runtime~main.229c360f.js"
   },
   {
-    "revision": "f33a1e062bd40ea60bb4",
-    "url": "/static/js/main.f33a1e06.chunk.js"
+    "revision": "4f1223442b506a47701c",
+    "url": "/static/js/main.4f122344.chunk.js"
   },
   {
     "revision": "e99ac9450d9cbf35e014",
     "url": "/static/js/1.e99ac945.chunk.js"
   },
   {
-    "revision": "af3dc3c055bf946edb78d5173ead6b41",
+    "revision": "e0d9a9a6b3049218c2ba5b7aa662af49",
     "url": "/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.2ff2bb3dece3dcf205382e55c4027da7.js"
+  "/precache-manifest.d455fcedd279e41753eedc902eb8f228.js"
 );
 
 workbox.clientsClaim();

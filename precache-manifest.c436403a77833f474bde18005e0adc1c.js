c584bdd5c289f3a8cee4a2ee3b36399a3261a12b
Ned Schwartz <ned@theinterned.net>
2018-10-17 13:13:15 -0400

    Updates

---
+++ precache-manifest.c436403a77833f474bde18005e0adc1c.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "5e30bc09f51b1b90d630",
+    "url": "/static/js/main.5e30bc09.chunk.js"
+  },
+  {
+    "revision": "e99ac9450d9cbf35e014",
+    "url": "/static/js/1.e99ac945.chunk.js"
+  },
+  {
+    "revision": "9598edf0a60cfbe7bfb46c4556e01430",
+    "url": "/index.html"
+  }
+];
